47c337585e796d19cc9956ac23a1a8fab3ce5685
jwlee <jwlee@ex-em.com>
2018-11-21 15:03:22 +0900

    [#209] Chart Virtual DOM Converting ########## examples 변경

--- examples/routers/chart.vue
+++ examples/routers/chart.vue
@@ -1,20 +1,14 @@
 <template>
-  <div style="width: 100%; height: 100%; display: table;">
-    <div class="ev-chart-ttt">
-      <line-chart/>
-    </div>
+  <div style="width: 100%; height: 100%; display: block;">
+    <line-chart/>
   </div>
 </template>
 <script>
   import LineChart from './charts/chart.line';
-  import BarChart from './charts/chart.bar';
-  import PieChart from './charts/chart.pie';
 
   export default {
     components: {
       LineChart,
-      BarChart,
-      PieChart,
     },
     data() {
       return {
@@ -23,9 +17,4 @@
   };
 </script>
 <style>
-  .ev-chart-ttt {
-    width: 50%;
-    height: 100%;
-    display: table-cell;
-  }
 </style>
--- examples/routers/charts/chart.line.vue
+++ examples/routers/charts/chart.line.vue
@@ -6,7 +6,7 @@
   />
 </template>
 <script>
-  import chart from '@/components/chart';
+  import chart from '@/components/chart2';
 
   export default {
     components: {
@@ -23,14 +23,13 @@
           groups: [
             ['series1', 'series3'],
           ],
-          data: [
-            ['x',
-              '2017/01/01 00:00:00', '2017/01/01 00:01:00', '2017/01/01 00:02:00',
+          data: {
+            x: ['2017/01/01 00:00:00', '2017/01/01 00:01:00', '2017/01/01 00:02:00',
               '2017/01/01 00:03:00', '2017/01/01 00:04:00'],
-            ['series1', 100, 150, 50, 200, 350],
-            ['series2', 200, 100, null, 300, 400],
-            ['series3', 150, 0, 0, 350, 450],
-          ],
+            series1: [100, 150, 50, 200, 350],
+            series2: [200, 100, null, 300, 400],
+            series3: [150, 0, 0, 350, 450],
+          },
         },
         lineOptions: {
           type: 'line',
@@ -62,12 +61,63 @@
         },
       };
     },
-    destroyed() {
-      if (this.interval) {
-        clearTimeout(this.interval);
-      }
+    mounted() {
+      this.interval = setTimeout(() => this.updateData(), 2000);
+      this.interval2 = setTimeout(() => this.updateData2(), 4000);
+      this.interval3 = setTimeout(() => this.updateData3(), 6000);
+      this.interval4 = setTimeout(() => this.updateData4(), 8000);
+      this.interval5 = setTimeout(() => this.updateData5(), 10000);
     },
     methods: {
+      updateData() {
+        this.$refs.defaultChart.updateChartOptions({
+          title: {
+            text: 'Update Title',
+            style: {
+              color: '#0BA',
+            },
+          },
+          width: '40%',
+        });
+
+        if (this.interval) {
+          clearTimeout(this.interval);
+        }
+      },
+      updateData2() {
+        this.lineOptions.title.text = 'Update Title 2';
+        this.lineOptions.title.style = { color: '#F00' };
+        this.lineOptions.width = '30%';
+
+        if (this.interval2) {
+          clearTimeout(this.interval2);
+        }
+      },
+      updateData3() {
+        this.$refs.defaultChart.addAxisData('x', '2017-01-01 00:05:00');
+        this.$refs.defaultChart.addGraphData('series2', 1000);
+
+        if (this.interval3) {
+          clearTimeout(this.interval3);
+        }
+      },
+      updateData4() {
+        this.$refs.defaultChart.addDataSet({
+          x: '2017/01/01 00:06:00',
+          series3: 300,
+        });
+
+        if (this.interval4) {
+          clearTimeout(this.interval4);
+        }
+      },
+      updateData5() {
+        this.lineOptions.title.show = false;
+
+        if (this.interval5) {
+          clearTimeout(this.interval5);
+        }
+      },
     },
   };
 </script>
